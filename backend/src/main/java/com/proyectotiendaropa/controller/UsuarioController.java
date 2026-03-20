0621a8ca971fbc6df4dddcbf1f98c90d44fabeaa
David <davidvela1712@gmail.com>
2026-03-20 21:41:26 +0100

    Añadido get de usuarios

--- backend/src/main/java/com/proyectotiendaropa/controller/UsuarioController.java
+++ backend/src/main/java/com/proyectotiendaropa/controller/UsuarioController.java
@@ -3,10 +3,9 @@ package com.proyectotiendaropa.controller;
 import com.proyectotiendaropa.model.Usuario;
 import com.proyectotiendaropa.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -14,6 +13,11 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> listarUsuarios () {
+        return usuarioService.findAll();
+    }
 
     @PostMapping
     public Usuario crearUsuario (@RequestBody Usuario usuario) {
